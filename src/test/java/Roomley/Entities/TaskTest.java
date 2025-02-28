72cc5a0c81b30a5a0c20b87e4da4e2716034da48
Koi-IT <ajohnson104@madisoncollege.edu>
2025-02-28 12:58:39 -0600

    Added the task javabean, task test, and database.properties files

---
+++ src/test/java/Roomley/Entities/TaskTest.java
@@ -0,0 +1,33 @@
+package Roomley.Entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TaskTest {
+
+    @org.junit.jupiter.api.Test
+    void task() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTaskName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTaskName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTaskStatus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTaskStatus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTaskDescription() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTaskDescription() {
+    }
+}
