956fefcb4c67453ffea79f1c1e55603f455325fe
Koi-IT <ajohnson104@madisoncollege.edu>
2025-02-28 12:58:39 -0600

    Added the task javabean, task test, and database.properties files

---
+++ src/main/java/Roomley/Entities/Task.java
@@ -0,0 +1,37 @@
+package Roomley.Entities;
+
+/**
+ *
+ */
+public class Task {
+
+    Task task;
+    String taskName;
+    boolean taskStatus;
+    String taskDescription;
+
+    public void Task(){
+
+    }
+
+    public String getTaskName() { return this.taskName; }
+
+    public void setTaskName(String taskName) {
+
+    }
+
+    public boolean getTaskStatus() { return this.taskStatus; }
+
+    public void setTaskStatus(boolean taskStatus) {
+
+    }
+
+    public String getTaskDescription() { return this.taskDescription; }
+
+    public void setTaskDescription(String taskDescription) {
+
+
+    }
+
+
+}
