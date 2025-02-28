a8087e0e3b5d91345b948d76f38be3b7ce38026b
Koi-IT <ajohnson104@madisoncollege.edu>
2025-02-28 13:50:08 -0600

    Added SessionFactoryProvider and TaskDao

---
+++ src/main/java/Roomley/persistence/TaskDao.java
@@ -0,0 +1,31 @@
+package Roomley.persistence;
+
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import Roomley.entities.Task;
+import java.util.List;
+
+
+public class TaskDao {
+
+    public void createTask(){
+
+    }
+
+    public void deleteTask() {
+
+    }
+
+    public void updateTask() {
+
+    }
+
+    public void insertTask() {
+
+    }
+
+//    public List<Task> getAllTasks() {
+//
+//    }
+
+}
