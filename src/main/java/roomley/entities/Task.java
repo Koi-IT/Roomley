01fd343aeee87dfcb120fe479e2cdb951f590da1
Koi-IT <ajohnson104@madisoncollege.edu>
2025-03-01 12:32:12 -0600

    Added javadocs

--- src/main/java/roomley/entities/Task.java
+++ src/main/java/roomley/entities/Task.java
@@ -21,10 +21,19 @@ public class Task {
     @Column(name = "task_description")
     private String taskDescription;
 
+    /**
+     * No argument constructor for Task
+     */
     public Task () {
 
     }
 
+    /**
+     * Constructor to build Task object
+     * @param taskName        The name of the Task
+     * @param taskDescription The description of the Task
+     * @param taskStatus      The status of the Task
+     */
     public Task (String taskName,  String taskDescription, boolean taskStatus) {
         this.taskName = taskName;
         this.taskDescription = taskDescription;
@@ -32,22 +41,54 @@ public class Task {
 
     }
 
+    /**
+     * Gets the task name
+     * @return Task name
+     */
     public String getTaskName() { return this.taskName; }
 
+    /**
+     * Sets the task name
+     * @param taskName Task name
+     */
     public void setTaskName(String taskName) { this.taskName = taskName; }
 
+    /**
+     * Gets task status
+     * @return Task status
+     */
     public boolean getTaskStatus() { return this.taskStatus; }
 
+    /**
+     * Sets tast status
+     * @param taskStatus Task status
+     */
     public void setTaskStatus(boolean taskStatus) {
         this.taskStatus = taskStatus;
     }
 
+    /**
+     * Gets task description
+     * @return Task description
+     */
     public String getTaskDescription() { return this.taskDescription; }
 
+    /**
+     * Sets task description
+     * @param taskDescription Task description
+     */
     public void setTaskDescription(String taskDescription) { this.taskDescription = taskDescription; }
 
+    /**
+     * Gets task id
+     * @return Task id
+     */
     public int getId() { return this.id; }
 
+    /**
+     * Sets task id
+     * @param id Task id
+     */
     public void setId(int id) { this.id = id; }
 
 }
