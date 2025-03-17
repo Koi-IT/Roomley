d50b61fbb5f291c338c608718613674ddbb6c64f
Koi-IT <ajohnson104@madisoncollege.edu>
2025-03-17 15:24:46 -0500

    Updated insert test and added equals override to User entity

--- src/test/java/roomley/persistence/UserDaoTest.java
+++ src/test/java/roomley/persistence/UserDaoTest.java
@@ -4,7 +4,7 @@ import roomley.entities.Task;
 import roomley.entities.User;
 import roomley.util.Database;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,8 +22,8 @@ class UserDaoTest {
     @Test
     void getById() {
         User retrievedUser = this.userDao.getById(5);
-        Assertions.assertNotNull(retrievedUser);
-        Assertions.assertEquals("garry", retrievedUser.getUserFirstName());
+        assertNotNull(retrievedUser);
+        assertEquals("garry", retrievedUser.getUserFirstName());
     }
 
     @Test
@@ -32,28 +32,22 @@ class UserDaoTest {
         userUpdated.setUserFirstName("jake");
         this.userDao.update(userUpdated);
         User retrieveUserUpdate = this.userDao.getById(5);
-        Assertions.assertEquals(userUpdated.getUserFirstName(), "jake");
+        assertEquals("jake", userUpdated.getUserFirstName());
     }
 
     @Test
     void insert() {
         User userToInsert = new User("Student", "Password", "First","Last", "2001-01-01", "Student@user.com", 1, "user", "household", 1);
         int insertedUserId = this.userDao.insert(userToInsert);
-        Assertions.assertNotEquals(0, insertedUserId);
-        User inserteduser = this.userDao.getById(insertedUserId);
-        Assertions.assertEquals("First", inserteduser.getUserFirstName());
-        Assertions.assertEquals("Last", inserteduser.getUserLastName());
-        Assertions.assertEquals("Student", inserteduser.getUsername());
-        Assertions.assertEquals("24", inserteduser.getAge());
-        Assertions.assertEquals("Student@user.com", inserteduser.getUserEmail());
-        Assertions.assertEquals("user", inserteduser.getUserType());
+
+        assertEquals(userToInsert, this.userDao.getById(insertedUserId));
     }
 
     @Test
     void delete() {
         User userToDelete = this.userDao.getById(4);
         this.userDao.delete(userToDelete);
-        Assertions.assertNull(this.userDao.getById(4));
+        assertNull(this.userDao.getById(4));
     }
 
     @Test
@@ -64,25 +58,26 @@ class UserDaoTest {
         List<Task> tasks = userToDelete.getTasks();
         int TaskNumber1 = (tasks.get(0)).getTaskId();
         userDao.delete(userToDelete);
-        Assertions.assertNull(userDao.getById(5));
-        Assertions.assertNull(taskDao.getById(TaskNumber1));
+        assertNull(userDao.getById(5));
+        assertNull(taskDao.getById(TaskNumber1));
     }
 
     @Test
     void getAll() {
         List<User> allUsers = this.userDao.getAllUsers();
-        Assertions.assertEquals(4, allUsers.size());
+        assertEquals(4, allUsers.size());
     }
 
     @Test
     void getByPropertyEqual() {
         List<User> firstNameUser = this.userDao.getByPropertyEqual("userFirstName", "perry");
-        Assertions.assertEquals("perry", ((User)firstNameUser.get(0)).getUserFirstName());
+        assertEquals("perry", ((User)firstNameUser.get(0)).getUserFirstName());
     }
 
     @Test
     void getByPropertyLike() {
         List<User> propertyLikeName = this.userDao.getByPropertyLike("userLastName", "green");
-        Assertions.assertEquals(2, propertyLikeName.size());
+        assertEquals(2, propertyLikeName.size());
     }
+
 }
