a1940fd739d4a8f865bcf4e1b9a112d9eb7c2234
Koi-IT <ajohnson@koi-dev.com>
2025-05-12 00:20:17 -0500

    Updated Roomley-DB

--- src/main/java/roomley/persistence/GenericDao.java
+++ src/main/java/roomley/persistence/GenericDao.java
@@ -16,44 +16,56 @@ public class GenericDao<T> {
 
     public GenericDao(Class<T> type) {
         this.type = type;
+
     }
 
     public T getById(int id) {
         try (Session session = sessionFactory.openSession()) {
             return session.get(type, id);
+
         }
     }
 
     public List<T> getAll() {
+
         try (Session session = sessionFactory.openSession()) {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<T> query = builder.createQuery(type);
             query.from(type);
             return session.createQuery(query).getResultList();
+
         }
+
     }
 
     public List<T> getByPropertyEqual(String propertyName, Object value) {
+
         try (Session session = sessionFactory.openSession()) {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<T> query = builder.createQuery(type);
             Root<T> root = query.from(type);
             query.select(root).where(builder.equal(root.get(propertyName), value));
             return session.createQuery(query).getResultList();
+
         }
+
     }
 
     public List<T> getByPropertyLike(String propertyName, Object value) {
+
         try (Session session = sessionFactory.openSession()) {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<T> query = builder.createQuery(type);
             Root<T> root = query.from(type);
             query.where(builder.like(root.get(propertyName), "%" + value + "%"));
             return session.createQuery(query).getResultList();
+
         }
+
     }
 
     public List<T> getByPropertiesEqual(Map<String, Object> properties) {
+
         if (properties.isEmpty()) {
             return new ArrayList<>();  // Return empty list if no properties are passed
         }
@@ -62,45 +74,59 @@ public class GenericDao<T> {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<T> query = builder.createQuery(type);
             Root<T> root = query.from(type);
-
             List<Predicate> predicates = new ArrayList<>();
+
             for (Map.Entry<String, Object> entry : properties.entrySet()) {
                 String[] keys = entry.getKey().split("\\.");
                 Path<Object> path = root.get(keys[0]);
+
                 for (int i = 1; i < keys.length; i++) {
                     path = path.get(keys[i]);
+
                 }
                 predicates.add(builder.equal(path, entry.getValue()));
+
             }
 
             query.select(root).where(builder.and(predicates.toArray(new Predicate[0])));
             return session.createQuery(query).getResultList();
+
         }
+
     }
 
-
     public T insert(T entity) {
+
         try (Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             session.persist(entity);
             transaction.commit();
             return entity;
+
         }
+
     }
 
     public void update(T entity) {
+
         try (Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             session.merge(entity);
             transaction.commit();
+
         }
+
     }
 
     public void delete(T entity) {
+
         try (Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             session.delete(entity);
             transaction.commit();
+
         }
+
     }
+
 }
